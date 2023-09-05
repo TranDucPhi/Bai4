41a5ad8d773096ab7cc9aa4593c37f1bdef9c570
MAY 06 <MAY 06@LAB2-02>
2023-09-05 14:34:34 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        txtdl = New TextBox()
+        txtal = New TextBox()
+        Button1 = New Button()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(205, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(347, 54)
+        Label1.TabIndex = 0
+        Label1.Text = "Tính Năm Âm Lịch"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(135, 213)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(128, 21)
+        Label2.TabIndex = 0
+        Label2.Text = "Năm Dương Lịch"
+        ' 
+        ' txtdl
+        ' 
+        txtdl.Location = New Point(135, 237)
+        txtdl.Name = "txtdl"
+        txtdl.Size = New Size(128, 23)
+        txtdl.TabIndex = 1
+        ' 
+        ' txtal
+        ' 
+        txtal.Location = New Point(516, 237)
+        txtal.Name = "txtal"
+        txtal.Size = New Size(104, 23)
+        txtal.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(362, 236)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "==>"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(516, 213)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(104, 21)
+        Label3.TabIndex = 0
+        Label3.Text = "Năm Âm Lịch"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(txtal)
+        Controls.Add(txtdl)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtdl As TextBox
+    Friend WithEvents txtal As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim soducan As New Integer
+        Dim soduchi As New Integer
+        Dim nam As Integer
+        nam = Integer.Parse(txtdl.Text)
+        Dim can As String
+        can = " "
+        soducan = (nam - 3) Mod 10
+        Select Case soducan
+            Case 0
+                can = "Quý"
+            Case 1
+                can = "Giáp"
+            Case 2
+                can = "Ất"
+            Case 3
+                can = "Bính"
+            Case 4
+                can = "Đinh"
+            Case 5
+                can = "Giáp"
+            Case 6
+                can = "Ất"
+            Case 7
+                can = "Canh"
+            Case 8
+                can = "Tân"
+            Case 9
+                can = "Nhâm"
+        End Select
+        Dim chi As String
+        chi = " "
+        soduchi = (nam - 3) Mod 12
+        Select Case soduchi
+            Case 0
+                chi = "Hợi"
+            Case 1
+                chi = "Tý"
+            Case 2
+                chi = "Sửu"
+            Case 3
+                chi = "Dần"
+            Case 4
+                chi = "Mão"
+            Case 5
+                chi = "Thìn"
+            Case 6
+                chi = "Tỵ"
+            Case 7
+                chi = "Ngọ"
+            Case 8
+                chi = "Mùi"
+            Case 9
+                chi = "Thân"
+            Case 10
+                chi = "Dậu"
+            Case 11
+                chi = "Tuất"
+        End Select
+        txtal.Text = can & " " & chi
+    End Sub
+End Class
